e93e166241fc8a0f6afcf7a6c3817cdfa6733df7
Alex Lee <alexhojonglee@gmail.com>
2022-09-22 14:55:52 +0900

    특정 게시물을 찾을 때 없는 경우 결과 값 처리

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v1 as uuid } from 'uuid';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -24,7 +24,13 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const found = this.boards.find((board) => board.id === id);
+
+    if (!found) {
+      throw new NotFoundException(`Not Found Board with id ${id}`);
+    }
+
+    return found;
   }
 
   deleteBoardById(id: string): void {
