62f229b7f150a9888372d4770cbdc91c78a56db1
Alex Lee <alexhojonglee@gmail.com>
2022-09-23 12:08:30 +0900

    유저 이름에 유니크한 값 주기

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,8 @@
-import { EntityRepository, Repository } from 'typeorm';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, QueryFailedError, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 
@@ -7,6 +11,14 @@ export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error instanceof QueryFailedError) {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
