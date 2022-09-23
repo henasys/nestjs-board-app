48f359b6a96d87de740bc5b9bc66789137ca917d
Alex Lee <alexhojonglee@gmail.com>
2022-09-23 17:01:41 +0900

    로그에 대해서

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  Logger,
   Param,
   ParseIntPipe,
   Patch,
@@ -23,10 +24,12 @@ import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipes
 @Controller('boards')
 @UseGuards(AuthGuard())
 export class BoardsController {
+  private logger = new Logger('Boards');
   constructor(private boardsService: BoardsService) {}
 
   @Get('/')
   getAllBoards(@GetUser() user: User): Promise<Board[]> {
+    this.logger.verbose(`User "${user.username}" trying to get all boards`);
     return this.boardsService.getAllBoards(user);
   }
 
@@ -36,6 +39,7 @@ export class BoardsController {
     @Body() createBoardDto: CreateBoardDto,
     @GetUser() user: User,
   ): Promise<Board> {
+    this.logger.verbose(`User "${user.username}" creating a new board`);
     return this.boardsService.createBoard(createBoardDto, user);
   }
 
